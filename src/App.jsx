08f3de5cf37bd809bf5e06efa2f2bc2c123a4ac3
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-11-08 23:08:32 +0800

    Add Select input to Input Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import InputBox from "./components/InputBox";
 import "./index.css";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
           "url(https://images.unsplash.com/photo-1580519542036-c47de6196ba5?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bW9uZXl8ZW58MHx8MHx8fDA%3D)",
       }}
     >
-      <h1 className="bg-red-200">Test for Tailwind</h1>
+      <InputBox label={"Twelve"} amount={199} />
     </div>
   );
 }
